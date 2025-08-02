1e2a858e95a302718d30b72f0b856600a87e76fc
saintsauceee <saintsaucee@gmail.com>
2025-08-02 18:11:30 -0400

    fk id

--- client/src/app/editor/page.tsx
+++ client/src/app/editor/page.tsx
@@ -1,10 +1,6 @@
 import Chat from "@/components/editor/chat/Chat";
 
-interface EditorPageProps {
-    params: { id: string };
-}
-
-export default function EditorPage({ params }: EditorPageProps) {
+export default function EditorPage() {
     return (
         <div className="flex flex-col w-screen h-screen p-2 bg-white">
             <Chat />
