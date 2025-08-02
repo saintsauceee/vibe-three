359ffc7d533151430867d126f85cbb9421661b26
saintsauceee <saintsaucee@gmail.com>
2025-08-02 17:56:07 -0400

    another fix

--- client/src/app/editor/[id]/page.tsx
+++ client/src/app/editor/[id]/page.tsx
@@ -3,7 +3,7 @@ import Chat from "@/components/editor/chat/Chat";
 export default async function EditorPage({ params }: { params: { id: string } }) {
     return (
         <div className="flex flex-col w-screen h-screen p-2 bg-white">
-            <Chat />
+            {/* <Chat /> */}
         </div>
     );
 }
